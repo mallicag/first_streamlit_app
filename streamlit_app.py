115e3dfda4757b7bd0d437e816879667cbc371dd
mallicag <137859173+mallicag@users.noreply.github.com>
2023-06-27 01:01:51 -0400

    Update streamlit_app.py
    new header added

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,5 @@ streamlit.title('Hello World')
 streamlit.header('Breakfast')
 streamlit.text('🥣Oatmeal')
 streamlit.text(' 🥑🍞 Avocado Sandwhich')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
