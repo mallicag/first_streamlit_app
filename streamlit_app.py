e35b3426181de6559ee1beee4985eda9f389b04d
mallicag <137859173+mallicag@users.noreply.github.com>
2023-06-28 02:21:25 -0400

    Update streamlit_app.py
    error hyndling

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.error import URLERROR
+from urllib.error import URLError
 
 streamlit.title('Hello World')
 streamlit.header('Breakfast')
@@ -24,16 +24,24 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+try:
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit_choice:
+  streamlit.error("Please select fruit")
+  else
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  streamlit.dataframe(fruityvice_normalized)
 streamlit.write('The user entered ', fruit_choice)
 
+except URLError as e
+  streamlit.error()
+
 #import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-
-
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+#streamlit.dataframe(fruityvice_normalized)
 streamlit.stop()
 
 #import snowflake.connector
